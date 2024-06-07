f00a426d9db156a11d08ec0ed583d986ba5a5087
Princelawrenz777 <164854270+Princelawrenz777@users.noreply.github.com>
2024-06-06 18:36:20 -0700

    Update data.js

--- data.js
+++ data.js
@@ -12,70 +12,70 @@ export const mapClasses = [
 ];
 export const previews = [
   {
-    img: "./assets/main-1.jpg",
+    img: "./assets/GIF_X.gif",
     title: "Ra",
-    tags: "Futuristic Fashion, Minimal Design",
+    tags: "Digital Generative AI, Virtual Band",
     description:
-      "Exploring the intersection of minimalism and future fashion trends in web design.",
+      "Your one stop shop for the world's definitive digital anime band.",
   },
   {
-    img: "./assets/main-2.jpg",
+    img: "./assets/GIF_Rangiku 2.gif",
     title: "Uptask",
     tags: "Fashion Innovation, Graphic Simplicity",
     description:
       "Innovative fashion-forward web design with a core focus on simplicity and elegance.",
   },
   {
-    img: "./assets/main-3.jpg",
+    img: "./assets/ORIHIME DJ.gif",
     title: "Genix",
     tags: "Eco-Fashion, Sustainable Design",
     description:
       "Sustainable fashion meets modern web aesthetics, highlighting eco-friendly apparel innovations.",
   },
   {
-    img: "./assets/main-4.jpg",
+    img: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYnUwdGdxaDU3OHV4bGxkOHNsdjEzYm9iN3h3bnJvM2U1YjhmN2VnOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vBGSvV0hLHxpjAWIqv/giphy.gif",
     title: "Unor",
     tags: "Digital Fashion, UI/UX",
     description:
       "Digital-first fashion branding that merges cutting-edge UI/UX principles with style.",
   },
   {
-    img: "./assets/main-5.jpg",
+    img: "./assets/SOIFON PHASE 1 ANIMATION (2024).gif",
     title: "Xav",
     tags: "Interactive Design, Fashion Tech",
     description:
       "Tech-infused fashion experiences showcased through interactive web design elements.",
   },
   {
-    img: "./assets/main-6.jpg",
+    img: "./assets/ezgif-2-b1ebce3a21.gif",
     title: "Maxim Stark",
     tags: "Futuristic Wearables, Web Graphics",
     description:
       "Futuristic wearables and gear presented in a sleek, graphically rich web interface.",
   },
   {
-    img: "./assets/main-7.jpg",
+    img: "./assets/ezgif-1-1e24f16b64.gif",
     title: "Pitcher",
     tags: "Innovative Textiles, Web Presentation",
     description:
       "Web presentation of innovative textiles that redefine the boundaries of modern fashion.",
   },
   {
-    img: "./assets/main-8.jpg",
+    img: "./assets/ezgif-4-4f6831dfff.gif",
     title: "MindSpace",
     tags: "Augmented Reality, Fashion Design",
     description:
       "Augmented reality in fashion design, creating immersive web experiences for users.",
   },
   {
-    img: "./assets/main-9.jpg",
+    img: "https://i.imgur.com/3puvQnv.jpeg",
     title: "The Athletix",
     tags: "Virtual Fashion, Graphic Design",
     description:
       "Virtual fashion collections displayed through stunning graphic design and web aesthetics.",
   },
   {
-    img: "./assets/main-10.jpg",
+    img: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHFlNXc0M2VybW9jN2d2d290YXUzMnMzeXp6d2dzaDU2cGpkOWt5diZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SaxRfitPapDgplsYlE/giphy.gif",
     title: "All Work",
     tags: "Minimalist Aesthetics, Fashion Forward",
     description:
