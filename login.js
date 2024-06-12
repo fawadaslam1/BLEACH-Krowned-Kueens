012dcbf20f39cfbfcdc28e6d0467cec3405333ed
Princelawrenz777 <164854270+Princelawrenz777@users.noreply.github.com>
2024-06-12 12:49:23 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,25 +1,26 @@
-/*=============== SHOW MENU ===============*/
+/*===== LOGIN SHOW and HIDDEN =====*/
+const signUp = document.getElementById('sign-up'),
+    signIn = document.getElementById('sign-in'),
+    loginIn = document.getElementById('login-in'),
+    loginUp = document.getElementById('login-up')
 
 
-/* Menu show */
+signUp.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
 
+    // Add classes
+    loginIn.classList.toggle('none')
+    loginUp.classList.toggle('block')
+})
 
-/* Menu hidden */
+signIn.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
 
-
-/*=============== SEARCH ===============*/
-
-
-/* Search show */
-
-
-/* Search hidden */
-
-
-/*=============== LOGIN ===============*/
-
-
-/* Login show */
-
-
-/* Login hidden */
+    // Add classes
+    loginIn.classList.toggle('block')
+    loginUp.classList.toggle('none')
+})
