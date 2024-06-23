8316fb84a1e9af3a0ee2e4d80732a381ae8b38c8
Princelawrenz777 <164854270+Princelawrenz777@users.noreply.github.com>
2024-06-23 15:38:56 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,47 @@
 <?php 
-session_start();
+  session_start(); 
 
-	include("connection.php");
-	include("functions.php");
-
-	$user_data = check_login($con);
-
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
 ?>
-
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="https://i.imgur.com/FHnHqMh.jpeg" type="image/x-icon">
-      
-    <title>WELCOME - BLEACH: KROWNED KUEENS</title>
-    </head>
-    <body>
+<html>
+<head>
+	<title>HOME - KROWNED KUEENS MUSIC PROJECT</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
 
-	<a href="logout.php">Logout</a>
-	<h1>Welcome to the Krowned Kueens Music Project! 
-	What would you like to do today?</h1>
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+	Welcome to the Krowned Kueens Music Project page!	
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php 
+          	echo $_SESSION['success']; 
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
 
-	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+
 </body>
 </html>
