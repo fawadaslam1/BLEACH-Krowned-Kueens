be26638f01c2512e145783391e961d8e834f3d4e
Princelawrenz Hamlin <164854270+Princelawrenz777@users.noreply.github.com>
2024-06-29 17:06:21 -0700

    Update server.php

--- server.php
+++ server.php
@@ -31,20 +31,16 @@ if (isset($_POST['reg_user'])) {
 
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-
-  if ($user) { // if user exists
-    if ($user['username'] === $username) {
-      array_push($errors, "Username already exists");
+if ($result) {
+    $user = mysqli_fetch_assoc($result);
+    if ($user) {
+        // Check username and email existence
     }
-
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
-    }
-  }
-
+} else {
+    // Handle query execution error
+    echo "Error executing query: " . mysqli_error($db);
+}
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
